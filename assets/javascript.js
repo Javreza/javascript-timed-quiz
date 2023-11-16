d6f29d06d934e8ded48d7547882bb3ec3b58e1c4
jvrza <jvrza20@gmail.com>
2023-11-16 10:36:50 -0600

    Added some base HTML and Javascript elements.

---
+++ assets/javascript.js
@@ -0,0 +1,10 @@
+var start = document.getElementById("start");
+var assesment = document.getElementById("assesment");
+var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+
